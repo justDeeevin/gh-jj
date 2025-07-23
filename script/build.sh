283dbe4722ad2a855a3f8a69e033252c5b9222bc
Devin Droddy <devin@justdeeevin.dev>
2025-07-23 18:15:30 -0400

    initialize crate

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-echo "TODO implement this script."
-echo "It should build binaries in dist/<platform>-<arch>[.exe] as needed."
-exit 1
+mkdir -p ./dist
+
+TARGET_TRIPLE=${TARGET_TRIPLE:-x86_64-unknown-linux-gnu}
+GOOS_GOARCH=${GOOS_GOARCH:-linux-amd64}
+
+cargo build --release --locked --target "${TARGET_TRIPLE}"
+mv "target/${TARGET_TRIPLE}/release/gh-jj" "./dist/gh-jj-${GOOS_GOARCH}"
